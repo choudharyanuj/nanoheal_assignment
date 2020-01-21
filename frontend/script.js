673bd499092aaeaf60326bff0584d0e1fe27901d
choudharyanuj <anuj-choudhary@outlook.com>
2020-01-21 13:08:45 +0530

    Added Redme File

---
+++ frontend/script.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','http://127.0.0.1:5000/home')
+    xhr.send()
+    xhr.onload = function(){
+        if(xhr.data !== null) {
+            var datas = JSON.parse(xhr.response)
+            console.log(datas)
+        }
+        else{
+            console.log("Data Not Found")
+        }
+        datas.map(function(element){
+            $("#display").append(
+                "<div id ='allmovie'>" + 
+                    "<h1>Name:" + element.name +"</h1>" + 
+                    "<h2>Category: " + element.category + "</h2>" +
+                    "<h5 class='description'>Description: " +element.description+ "</h5>" + 
+                    "<hr></hr>" +
+                "</div>"
+            );
+        });
+    }
+}
+
+function search() {
+    var data = document.getElementById('data').value
+    console.log(data)
+    var xhrsearch = new XMLHttpRequest();
+    var url ='http://127.0.0.1:5000/search'
+    var params = 'data'
+    xhrsearch.open('POST',url)
+    xhrsearch.send()
+    xhrsearch.onload = function(){
+        if(xhrsearch.data !== null) {
+            var datas = JSON.parse(xhrsearch.response)
+            console.log(datas)
+        }
+        else{
+            alert("Data Not Found")
+        }
+        datas.map(function(element){
+            $("#display").append(
+                "<div id ='allmovie'>" + 
+                    "<h1>Name:" + element.name +"</h1>" + 
+                    "<h2>Category: " + element.category + "</h2>" +
+                    "<h5 class='description'>Description: " +element.description+ "</h5>" + 
+                    "<hr></hr>" +
+                "</div>"
+            );
+        });
+    }
+}
